268dfe81a571c1444157c20b180affddd0252970
Ce-bit123 <118348146+Ce-bit123@users.noreply.github.com>
2023-01-06 16:51:43 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -43,5 +43,5 @@ if st.button("Submit"):
     P = mpimg.imread("shap_force_plot.png")
     st.image(P, caption="shap_force_plot", channels="RGB")
     st.text(f"This patient has a higher probability of {prediction} within 72 hours")
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X_display.iloc[0,:]), height=200, width=1000)
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X_display.iloc[:1000,:]), height=400, width=1000)
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X.iloc[0,:]), height=200, width=1000)
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[:1000,:], X.iloc[:1000,:]), height=400, width=1000)
