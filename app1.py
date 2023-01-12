abd8c7d58e4d4433f0d1aebca3019345ab5cd610
Ce-bit123 <118348146+Ce-bit123@users.noreply.github.com>
2023-01-12 12:42:49 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -46,7 +46,7 @@ if st.button("Submit"):
     #st_shap(shap.plots.waterfall(shap_values[0]), height=300)
     #st_shap(shap.plots.beeswarm(shap_values), height=300)
     st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X.iloc[0,:]), height=200, width=700)
-    if(prediction = 0):
+    if prediction = 0:
     st.text(f"This patient has a higher probability of Non-END within 72 hours")
     else:
     st.text(f"This patient has a higher probability of END within 72 hours")
